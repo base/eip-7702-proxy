75e4786ad030e2f06bc8f5197ea1bbe2b8534f49
qoog <gophers-brats1a@icloud.com>
2025-03-09 12:42:31 +0300

    chore: Fix typo in interface description

--- src/interfaces/IAccountStateValidator.sol
+++ src/interfaces/IAccountStateValidator.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.23;
 bytes4 constant ACCOUNT_STATE_VALIDATION_SUCCESS = 0xccd10cc8; // bytes4(keccak256("validateAccountState(address,address)"))
 
 /// @title IAccountStateValidator
-/// @notice Interface for account-specific validation logi
+/// @notice Interface for account-specific validation logic
 /// @dev This interface is used to validate the state of a account after an upgrade
 /// @author Coinbase (https://github.com/base/eip-7702-proxy)
 interface IAccountStateValidator {
