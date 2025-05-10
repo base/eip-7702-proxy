ea5b9df633083e8e64ca6035b8cb728d32061a45
Amie Corso <amie.corso@coinbase.com>
2025-05-10 07:42:59 -0700

    modify deploy scripts for mocks/dmeos

--- scripts/DeployMocks.s.sol
+++ scripts/DeployMocks.s.sol
@@ -11,19 +11,22 @@ import {MockImplementation} from "../test/mocks/MockImplementation.sol";
  * @dev Before deploying contracts, make sure dependencies have been installed at the latest or otherwise specific
  * versions using `forge install [OPTIONS] [DEPENDENCIES]`.
  *
- * forge script scripts/DeployMocks.s.sol:DeployMocks --account odyssey-deployer --sender $SENDER --rpc-url $ODYSSEY_RPC --broadcast -vvvv
+ * forge script scripts/DeployMocks.s.sol:DeployMocks --account spmdeployer --sender $SPM_DEPLOYER --rpc-url $BASE_SEPOLIA_RPC --broadcast -vvvv --verify --verifier-url $SEPOLIA_BASESCAN_API --etherscan-api-key $BASESCAN_API_KEY
+ * forge script scripts/DeployMocks.s.sol:DeployMocks --account spmdeployer --sender $SPM_DEPLOYER --rpc-url $BASE_RPC --broadcast -vvvv --verify --verifier-url $BASESCAN_API --etherscan-api-key $BASESCAN_API_KEY
  */
 contract DeployMocks is Script {
+    uint256 constant SALT = 1;
+
     function run() external {
         vm.startBroadcast();
 
         // 1. Deploy mock implementation
-        MockImplementation mockImplementation = new MockImplementation();
+        MockImplementation mockImplementation = new MockImplementation{salt: bytes32(uint256(SALT))}();
 
         vm.stopBroadcast();
 
         // Log deployed addresses
         console.log("Deployed addresses:");
-        console.log("MockImplementation:", address(mockImplementation));
+        console.log("MockImplementation:", address(mockImplementation)); //0x011056384Cb0C3F6B999A65d9f664a835961FFe3 and has been deployed on Base and Base Sepolia
     }
 }
--- scripts/DeployStorageEraser.s.sol
+++ scripts/DeployStorageEraser.s.sol
@@ -8,19 +8,24 @@ import {MultiOwnableStorageEraser} from "../src/MultiOwnableStorageEraser.sol";
 /**
  * @notice Deploy a storage eraser contract.
  *
- * forge script scripts/DeployStorageEraser.s.sol:DeployStorageEraser --account odyssey-deployer --sender $SENDER --rpc-url $ODYSSEY_RPC --broadcast -vvvv
+ * forge script scripts/DeployStorageEraser.s.sol:DeployStorageEraser --account spmdeployer --sender $SPM_DEPLOYER --rpc-url $BASE_SEPOLIA_RPC --broadcast -vvvv --verify --verifier-url $SEPOLIA_BASESCAN_API --etherscan-api-key $BASESCAN_API_KEY
+ * forge script scripts/DeployStorageEraser.s.sol:DeployStorageEraser --account spmdeployer --sender $SPM_DEPLOYER --rpc-url $BASE_RPC --broadcast -vvvv --verify --verifier-url $BASESCAN_API --etherscan-api-key $BASESCAN_API_KEY
+ * forge verify-contract --chain-id 84532 --num-of-optimizations 20000 --watch --etherscan-api-key $BASESCAN_API_KEY --compiler-version v0.8.23 0xf88cBE56c3b636747AD8FF21890A6B96954eE5E8 MultiOwnableStorageEraser --verifier-url $SEPOLIA_BASESCAN_API
  */
 contract DeployStorageEraser is Script {
+    uint256 constant SALT = 1;
+
     function run() external {
         vm.startBroadcast();
 
         // 1. Deploy storage eraser
-        MultiOwnableStorageEraser multiOwnableStorageEraser = new MultiOwnableStorageEraser();
+        MultiOwnableStorageEraser multiOwnableStorageEraser =
+            new MultiOwnableStorageEraser{salt: bytes32(uint256(SALT))}();
 
         vm.stopBroadcast();
 
         // Log deployed addresses
         console.log("Deployed addresses:");
-        console.log("MultiOwnableStorageEraser:", address(multiOwnableStorageEraser));
+        console.log("MultiOwnableStorageEraser:", address(multiOwnableStorageEraser)); // 0xf88cBE56c3b636747AD8FF21890A6B96954eE5E8 and has been deployed on Base and Base Sepolia
     }
 }
